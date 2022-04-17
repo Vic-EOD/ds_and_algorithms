0a15b4828989346304ef342ca9b2fec1731ee3ba
Victor Martinez <victor.eod2@gmail.com>
2022-04-16 22:52:16 -0700

    insertion sort and update project structure

---
+++ src/main/algorithms/Main.java
@@ -0,0 +1,19 @@
+import sorting.InsertionSort;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22};
+
+        for(int i = 0; i < intArray.length; i++) {
+            System.out.print(intArray[i] + ", ");
+        }
+
+        InsertionSort.sort(intArray);
+        System.out.println();
+
+        for(int i = 0; i < intArray.length; i++) {
+            System.out.print(intArray[i] + ", ");
+        }
+    }
+}
