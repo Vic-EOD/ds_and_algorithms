a70d4aac450f0ce8de2889c73c3ee55e3c5880f6
Victor Martinez <victor.eod2@gmail.com>
2022-05-15 17:48:12 -0700

    selection sort

--- src/main/algorithms/sorting/SelectionSort.java
+++ src/main/algorithms/sorting/SelectionSort.java
@@ -1,5 +1,16 @@
+package sorting;
 public class SelectionSort {
-	public void sort(int[] array){
-
+	public static void sort(int[] array){
+        for(int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            int largest = 0;
+            for(int i = 1; i < lastUnsortedIndex; i++) {
+                if(array[i] > array[largest]) {
+                    largest = i; 
+                }
+            }
+            int temp = array[lastUnsortedIndex];
+            array[lastUnsortedIndex] = array[largest];
+            array[largest] = temp;
+        }
 	}
 }
